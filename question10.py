30a2e7547851a389d5641ae8f7d513446cd20510
NKATEKO01 <67948466+NKATEKO01@users.noreply.github.com>
2021-09-22 20:09:06 +0200

    Update question10.py

--- question10.py
+++ question10.py
@@ -3,7 +3,8 @@ def common_letter(str1,str2):#print common letter
     for i in str1:
         for k in str2:
             if i.lower() == k.lower():
-                file.append(i)
+                if i not in file:
+                    file.append(i)
 
     return ",".join(file)
 print("common letters: " + common_letter("house","computers"))
