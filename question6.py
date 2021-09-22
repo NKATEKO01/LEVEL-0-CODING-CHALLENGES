196d0bc3fb9657c3c413b5062cc672773f829845
NKATEKO01 <67948466+NKATEKO01@users.noreply.github.com>
2021-09-22 16:31:09 +0200

    Update question6.py

--- question6.py
+++ question6.py
@@ -10,13 +10,3 @@ def max_num(a,b,c,):#print max number
       return b
 
 print (max_num(1,33,8))
-
-#bonus
-def maximus(*t):#print max number
-    max=t[0]
-    for i in t:
-        if i > t:
-            t=i
-    return max
-
-print(max(1,288.5547,21,3))
