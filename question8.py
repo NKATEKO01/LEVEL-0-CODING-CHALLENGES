8f01ba2d41f248f25b1d607fd46dfaac122bbaba
NKATEKO01 <67948466+NKATEKO01@users.noreply.github.com>
2021-09-29 12:54:38 +0200

    Update question8.py

--- question8.py
+++ question8.py
@@ -1,23 +1,21 @@
-x = 301
-a = "hour"
-b = "hours"
-c = "minutes"
-d = "minute"
-hour = x//60
-minutes = x%60
-
 def number_convert(x):
-    if hour > 1 and minutes<=1:
+    a = "hour"
+    b = "hours"
+    c = "minutes"
+    d = "minute"
+    hour = x//60
+    minutes = x%60
+    
+    if hour > 1 and minutes == 1:
         print(str(hour,),(b) +", "+ str(minutes),(d))
 
-    if hour > 1 and minutes>1:
+    if (hour > 1 or hour == 0) and (minutes>1 or minutes ==0):
         print(str(hour,),(b) +", "+ str(minutes),(c))
 
-    if  hour < 2 and minutes >1:
+    if  hour ==1 and (minutes >1 or minutes ==0):
         print(str(hour,),(a) +", "+ str(minutes),(c)) 
 
-    if hour < 2 and minutes<=1:
+    if hour == 1 and minutes==1:
         print(str(hour,),(a) +", "+ str(minutes),(d))
-
 
 number_convert(x)
