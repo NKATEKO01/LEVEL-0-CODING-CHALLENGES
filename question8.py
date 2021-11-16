1437c8668b3403dfefc7b6396603915fd42ea35d
NKATEKO01 <67948466+NKATEKO01@users.noreply.github.com>
2021-11-16 17:39:08 +0200

    Update question8.py

--- question8.py
+++ question8.py
@@ -3,19 +3,20 @@ def number_convert(x):
     b = "hours"
     c = "minutes"
     d = "minute"
-    hour = x//60
-    minutes = x%60
+    hour = x // 60
+    minutes = x % 60
     
     if hour > 1 and minutes == 1:
-        print(str(hour,),(b) +", "+ str(minutes),(d))
+        print(str(hour), b + ", " + str(minutes), d)
 
-    if (hour > 1 or hour == 0) and (minutes>1 or minutes ==0):
-        print(str(hour,),(b) +", "+ str(minutes),(c))
+    if (hour > 1 or hour == 0) and (minutes > 1 or minutes == 0):
+        print(str(hour), b + ", " + str(minutes), c)
 
-    if  hour ==1 and (minutes >1 or minutes ==0):
-        print(str(hour,),(a) +", "+ str(minutes),(c)) 
+    if  hour == 1 and (minutes >1 or minutes == 0):
+        print(str(hour), a + ",  "+ str(minutes), c) 
 
-    if hour == 1 and minutes==1:
-        print(str(hour,),(a) +", "+ str(minutes),(d))
+    if hour == 1 and minutes == 1:
+        print(str(hour), a + ", " + str(minutes), d)
 
+        
 number_convert(4)
