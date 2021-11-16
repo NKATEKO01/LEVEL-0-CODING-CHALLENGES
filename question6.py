f0462b8251ec1a2a727b11ea01bccd5f7b0c8771
NKATEKO01 <67948466+NKATEKO01@users.noreply.github.com>
2021-11-16 16:14:29 +0200

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,9 +1,14 @@
-def max_num(a,b,c,):
-    if (a > b and a > c):
+def max_num(
+    a,
+    b,
+    c,
+):
+    if a > b and a > c:
         return a
-    if (c > b and c > a):
+    if c > b and c > a:
        return c
-    if (b > a and b > c):
+    if b > a and b > c:
       return b
 
-print (max_num(1,33,8))
+
+print (max_num(1, 33, 8))
