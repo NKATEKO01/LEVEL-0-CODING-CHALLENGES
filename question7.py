70e39e4809229509d52cdf2d9f91a2fb36fc5a05
NKATEKO01 <67948466+NKATEKO01@users.noreply.github.com>
2021-11-16 16:16:58 +0200

    Update question7.py

--- question7.py
+++ question7.py
@@ -1,11 +1,11 @@
 def far_to_cel(x):
-    far = ((x - 32) * (5/9))
+    far = (x - 32) * (5 / 9)
     return far
 
-print(far_to_cel(15),"\xb0C")
+print(far_to_cel(15), "\xb0C")
 
 def cel_to_far(y):
-    cel = ((y * 9/5) + 32 )
+    cel = (y * 9 /5 ) + 32
     return cel
 
-print(cel_to_far(15),"\xb0F")
+print(cel_to_far(15), "\xb0F")
