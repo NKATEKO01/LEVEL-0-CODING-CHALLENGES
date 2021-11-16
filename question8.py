aaf27903a8a71af2aa09bb6bf266aebcd879c873
NKATEKO01 <67948466+NKATEKO01@users.noreply.github.com>
2021-11-16 17:42:17 +0200

    Update question8.py

--- question8.py
+++ question8.py
@@ -1,10 +1,10 @@
-def number_convert(x):
+def number_to_time(number):
     a = "hour"
     b = "hours"
     c = "minutes"
     d = "minute"
-    hour = x // 60
-    minutes = x % 60
+    hour = number // 60
+    minutes = number % 60
     
     if hour > 1 and minutes == 1:
         print(str(hour), b + ", " + str(minutes), d)
@@ -18,5 +18,5 @@ def number_convert(x):
     if hour == 1 and minutes == 1:
         print(str(hour), a + ", " + str(minutes), d)
 
-        
-number_convert(4)
+
+number_to_time(4)
