5bf3b61d5f12a772187e5b56a27688ca19d3e884
NKATEKO01 <67948466+NKATEKO01@users.noreply.github.com>
2021-11-16 16:05:56 +0200

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,5 +1,7 @@
-def triangle_area(a,b,c):
-    s = ((a + b + c)/2)
-    area = ((s*(s-a)*(s-b)*(s-c))**0.5)
+def triangle_area(a, b, c):
+    s = (a + b + c) / 2
+    area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
     return area
-print(triangle_area(3,4,5))
+
+
+print(triangle_area(3, 4 ,5))
