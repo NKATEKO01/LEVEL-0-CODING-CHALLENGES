9584579e45588106d5ada0ab4cc2a76ae4aa4249
NKATEKO01 <67948466+NKATEKO01@users.noreply.github.com>
2021-11-22 19:22:32 +0200

    Update question10.py

--- question10.py
+++ question10.py
@@ -1,12 +1,11 @@
 def common_letter(str1,str2):
-    file = []
+    common_letter_list = []
     for i in str1:
         for k in str2:
             if i.lower() == k.lower():
-                if i not in file:
-                    file.append(i)
-
-    return ",".join(file)
+                if i not in common_letter_list:
+                    common_letter_list.append(i)
+    return ", ".join(common_letter_list)
 
 
 print("common letters: " + common_letter("house", "computers"))
